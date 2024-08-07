a4a6adaec6ddf7a0165f6f63077c3148595473cc
Andrey Flegontov <forestheart87@gmail.com>
2024-08-07 13:32:54 +0300

    Fix Fatabse connection for Github action

--- config/initializer/database.rb
+++ config/initializer/database.rb
@@ -1,8 +1,12 @@
 require 'yaml'
+require 'erb'
 require 'sequel'
+require 'pry'
 
 env = ENV['RACK_ENV'] || 'development'
-config = YAML.load_file(File.expand_path('../../database.yml', __FILE__))[env]
+
+erb_out = ERB.new(File.read(File.expand_path('../../database.yml', __FILE__))).result
+config = YAML.load(erb_out)[env]
 
 DB = Sequel.connect(
   adapter: config['adapter'],
